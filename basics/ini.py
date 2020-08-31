be60132d5782c5883d6b9c6111e9aa1f4ec7a528
howardshih <howardshih617038@gmail.com>
2020-09-01 00:00:11 +0800

    Add ini utils

---
+++ basics/ini.py
@@ -0,0 +1,52 @@
+from configparser import ConfigParser
+
+
+def _to_config_format(obj) -> None or dict:
+    sections = dict()
+    if type(obj) is not dict:
+        return None
+    for section, section_content in obj.items():
+        options = dict()
+        if not hasattr(section, '__str__'):
+            return None
+        if type(section_content) is not dict:
+            return None
+        for key, value in section_content.items():
+            if not hasattr(key, '__str__'):
+                return None
+            if not hasattr(value, '__str__'):
+                return None
+            options[str(key)] = str(value)
+        sections[str(section)] = options
+    return sections
+
+
+def save_ini(obj, ini_path):
+    obj = _to_config_format(obj)
+    if obj is None:
+        return
+    config = ConfigParser()
+    for section, section_cont in obj.items():
+        config.add_section(section)
+        for key, value in section_cont.items():
+            config.set(section, key, value)
+    with open(ini_path, 'w') as ini_file:
+        config.write(ini_file)
+
+
+def load_ini(ini_path):
+    config = ConfigParser()
+    config.read(ini_path)
+    return {
+        section: {
+            key: value
+            for key, value in config.items(section)
+        } for section in config.sections()
+    }
+
+
+if __name__ == '__main__':
+    print(load_ini("Config.ini"))
+    save_ini({"A": {1: 2}, "B": {"abd": 1325}}, "out.ini")
+
+
